8d8dc1f5c04cb270f8e9d7f570983743cfec1222
NantenainaRld <nantenainarld@gmail.com>
2026-04-18 18:29:22 +0300

    add Patient model with attributes and getters/setters

---
+++ jsp_doctor_apointment_system/src/main/java/com/doctorapointment/dao/PatientDAO.java
@@ -0,0 +1,37 @@
+package com.doctorapointment.dao;
+
+import org.slf4j.LoggerFactory;
+
+import com.doctorapointment.model.Patient;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+
+public class PatientDAO {
+	private static final Logger log = LoggerFactory.getLogger(PatientDAO.class);
+
+	// insert patient
+	public boolean createPatient(Patient patient) {
+		String query = "INSERT INTO patient (id_pat, nom_pat, prenom_pat, date_nais, mdp_pat, email_pat)"
+				+ " VALUES (?, ?, ?, ?, ?, ?)";
+
+		try (Connection conn = DatabaseConnection.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(query);) {
+
+			stmt.setString(1, patient.getIdPat());
+			stmt.setString(2, patient.getNomPat());
+			stmt.setString(3, patient.getPrenomPat());
+			stmt.setObject(4, patient.getDateNais());
+			stmt.setString(5, patient.getMdpPat());
+			stmt.setString(6, patient.getEmailPat());
+
+			return stmt.executeUpdate() > 0;
+		} catch (SQLException e) {
+			log.error("Error creating patient : '{}' ", e);
+			return false;
+		}
+	}
+}
--- jsp_doctor_apointment_system/src/main/java/com/doctorapointment/model/Patient.java
+++ jsp_doctor_apointment_system/src/main/java/com/doctorapointment/model/Patient.java
@@ -1,5 +1,52 @@
 package com.doctorapointment.model;
 
-public class Patient {
+import java.time.LocalDate;
 
+public class Patient {
+	private String idPat, nomPat, prenomPat, mdpPat, emailPat;
+	private LocalDate dateNais;
+
+	// getters
+	public String getIdPat() {
+		return this.idPat;
+	}
+
+	public String getNomPat() {
+		return this.nomPat;
+	}
+
+	public String getPrenomPat() {
+		return this.prenomPat;
+	}
+
+	public String getMdpPat() {
+		return this.mdpPat;
+	}
+
+	public String getEmailPat() {
+		return this.emailPat;
+	}
+	public LocalDate getDateNais() {
+		return this.dateNais;
+	}
+	
+	// setters
+	public void setIdPat(String idPat) {
+		this.idPat = idPat;
+	}
+	public void setNomPat(String nomPat) {
+		this.nomPat = nomPat;
+	}
+	public void setPrenomPat(String prenomPat) {
+		this.prenomPat = prenomPat;
+	}
+	public void setMdpPat(String mdpPat) {
+		this.mdpPat = mdpPat;
+	}
+	public void setEmailPat(String emailPat) {
+		this.emailPat = emailPat;
+	}
+	public void setDateNais(LocalDate dateNais) {
+		this.dateNais = dateNais;
+	}
 }
