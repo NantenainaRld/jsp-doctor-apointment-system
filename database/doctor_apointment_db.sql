5601f8f2582ab4057f2b9e3a6a8fc7d1783a72b8
Nantenaina Rld <nantenainarld@gmail.com>
2026-05-01 20:22:41 +0300

    Update doctor_apointment_db.sql

--- database/doctor_apointment_db.sql
+++ database/doctor_apointment_db.sql
@@ -26,13 +26,13 @@ DROP TABLE IF EXISTS `disponibilite`;
 CREATE TABLE `disponibilite` (
   `id_dispo` int(11) NOT NULL AUTO_INCREMENT,
   `dispo_id_med` varchar(15) DEFAULT NULL,
-  `jour_dispo` int(11) NOT NULL DEFAULT 1,
   `debut_dispo` time NOT NULL,
   `fin_dispo` time NOT NULL,
+  `date_dispo` date NOT NULL,
   PRIMARY KEY (`id_dispo`),
   KEY `disponibilite_medecin_FK` (`dispo_id_med`),
   CONSTRAINT `disponibilite_medecin_FK` FOREIGN KEY (`dispo_id_med`) REFERENCES `medecin` (`id_med`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,6 +42,9 @@ CREATE TABLE `disponibilite` (
 SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
 LOCK TABLES `disponibilite` WRITE;
 /*!40000 ALTER TABLE `disponibilite` DISABLE KEYS */;
+INSERT INTO `disponibilite` VALUES
+(2,'M004','13:00:00','15:00:00','2026-05-02'),
+(4,'M004','11:32:00','13:32:00','2026-05-01');
 /*!40000 ALTER TABLE `disponibilite` ENABLE KEYS */;
 UNLOCK TABLES;
 COMMIT;
@@ -59,9 +62,9 @@ CREATE TABLE `medecin` (
   `nom_med` varchar(50) NOT NULL,
   `prenom_med` varchar(50) DEFAULT NULL,
   `specialite` varchar(50) NOT NULL,
-  `taux_horaire` int(11) NOT NULL DEFAULT 0,
   `lieu` varchar(50) NOT NULL,
   `mdp_med` varchar(255) NOT NULL,
+  `taux_horaire` decimal(20,2) NOT NULL,
   PRIMARY KEY (`id_med`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -73,6 +76,10 @@ CREATE TABLE `medecin` (
 SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
 LOCK TABLES `medecin` WRITE;
 /*!40000 ALTER TABLE `medecin` DISABLE KEYS */;
+INSERT INTO `medecin` VALUES
+('M001','FETRA','','oreille','fianarantsoa','$2a$10$X6eEPWRK6pLLTtgyH6QhwuR8.mqE8y6PZKJxRQNzpRrzxpShUjAnG',0.00),
+('M004','ROGER','Bary','Dentiste','Tanambao, Fianarantsoa','$2a$10$Yg5WR/UAQCL5hXqz.dK3xeT852Vpxi5FhQgRPHuGXplTd.8WX5j6K',10000.00),
+('M005','RAKOTO','','Cardiologue','Toliara','$2a$10$DpI.K/f188NFTdWtsQ.YeuzDBtcKC54T5rEiNGsTOYJvBh5.8/1/q',25000.00);
 /*!40000 ALTER TABLE `medecin` ENABLE KEYS */;
 UNLOCK TABLES;
 COMMIT;
@@ -86,7 +93,7 @@ DROP TABLE IF EXISTS `patient`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `patient` (
-  `id_pat` varchar(25) NOT NULL,
+  `id_pat` varchar(15) NOT NULL,
   `nom_pat` varchar(50) NOT NULL,
   `prenom_pat` varchar(50) DEFAULT NULL,
   `date_nais` date NOT NULL,
@@ -103,6 +110,12 @@ CREATE TABLE `patient` (
 SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+INSERT INTO `patient` VALUES
+('P006','MARCHEL','Jacque','2004-01-01','$2a$10$dQcJ8CtoT1NVsi19ogos6Oz5lguugMkU9DqG46Lo8ekjK2Iw00GqK','nantenainarld@gmail.com'),
+('P008','RALANDISON','Nantenaina','2002-02-01','$2a$10$ACfEv33/WCKRH6gxh1CxrewPrtnCPK2hGJ1szrux2ylLzeJL3R4rq','test@sasa.com'),
+('P009','NOM','','2004-02-05','$2a$10$d2FbBPrPAUJmovYfKv6ssObPMHSpKbHJk18qENoYLqq0KtWQzbyK6','nantenaina@gmail.com'),
+('P010','JACK','Maxim','2008-04-16','$2a$10$Yhf988beCoIVReW/0bAkk.RtKHANqAwkqEKH6M0CK68sq/lrQ.Q2S','maxim@gmail.com'),
+('P011','LOREM','Ipsum','2002-12-03','$2a$10$.Ba1jdiXpvp387qqWfLOKOidcu6cM2sWHNEyjOlNxSfoxfbB6KkMW','lorem@gmail.com');
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;
 COMMIT;
@@ -119,14 +132,17 @@ CREATE TABLE `rdv` (
   `id_rdv` int(11) NOT NULL AUTO_INCREMENT,
   `rdv_id_med` varchar(15) DEFAULT NULL,
   `rdv_id_pat` varchar(15) DEFAULT NULL,
-  `date_rdv` datetime NOT NULL,
+  `date_rdv` date NOT NULL,
   `etat_rdv` enum('en attente','confirmé','annulé') NOT NULL DEFAULT 'en attente',
+  `heure_debut` time NOT NULL,
+  `heure_fin` time NOT NULL,
+  `date_pris_rdv` datetime NOT NULL,
   PRIMARY KEY (`id_rdv`),
-  KEY `rdv_patient_FK` (`rdv_id_pat`),
   KEY `rdv_medecin_FK` (`rdv_id_med`),
+  KEY `rdv_patient_FK` (`rdv_id_pat`),
   CONSTRAINT `rdv_medecin_FK` FOREIGN KEY (`rdv_id_med`) REFERENCES `medecin` (`id_med`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `rdv_patient_FK` FOREIGN KEY (`rdv_id_pat`) REFERENCES `patient` (`id_pat`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,6 +152,9 @@ CREATE TABLE `rdv` (
 SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
 LOCK TABLES `rdv` WRITE;
 /*!40000 ALTER TABLE `rdv` DISABLE KEYS */;
+INSERT INTO `rdv` VALUES
+(4,'M005','P006','2026-05-02','confirmé','13:40:00','14:20:00','2026-04-25 21:59:32'),
+(6,'M004','P008','2026-05-02','confirmé','13:40:00','14:40:00','2026-04-29 19:01:44');
 /*!40000 ALTER TABLE `rdv` ENABLE KEYS */;
 UNLOCK TABLES;
 COMMIT;
@@ -154,4 +173,4 @@ SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2026-03-29 21:03:32
+-- Dump completed on 2026-05-01 19:49:22
